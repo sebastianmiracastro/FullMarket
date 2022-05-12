0ab9ef0a18653bc20e793b19fe5d16edeaec7947
anyiecon <anyiedcondiza@gmail.com>
2022-05-12 16:53:44 -0500

    UiHeaderRegister and StyleUIHeader done#1 [12052022]

---
+++ src/Components/UI/UIRegister/UIHeaderRegister.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RegisterImage from '../../../Image/RegisterFolderImage/RegisterImage.png'
+import StyleUIHeaderRegister from '../../../Styles/StyleRegister/StyleUIHeaderRegister.css'
+export const UIHeaderRegister=()=> {
+  return (
+    <div className='UiheaderRegister'> 
+      <div className='registerText'>
+        <h1 className='textRegisters'> REGISTRATE AQUÍ</h1>
+      </div>
+      <img className='registerImg' src={RegisterImage} ></img>
+      <div className='registerTexts'>
+        <p className='textRegister'>REGISTRATE AQUÍ TOTALMENTE GRATIS PARA UNA EXCELENTE EXPERIENCIA</p>
+      </div>
+    </div>
+  )
+}
