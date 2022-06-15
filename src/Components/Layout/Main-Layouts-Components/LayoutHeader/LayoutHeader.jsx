4e22c3ea14efb2a7b50c179a282e2ba373ef72d0
N_Sierra <nataliasierra0404@gmail.com>
2022-06-15 16:44:02 -0500

    Se añade buscador (Funcionando)

--- src/Components/Layout/Main-Layouts-Components/LayoutHeader/LayoutHeader.jsx
+++ src/Components/Layout/Main-Layouts-Components/LayoutHeader/LayoutHeader.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import '../../../Styles/Main-Styles/MainStyle.css'
 
-export const LayoutHeader = () => {
+export const LayoutHeader = ({event}) => {
   return (
     <>
-
       <div className="spacePrincipal"></div>
       <div className="containerSearch">
         <input
+        onChange={event}
           className="primarySearch"
           type="search"
           id="search"
           placeholder="   Buscar productos, categorías y más..."
         ></input>
-        <div className="iconSearch">
-          <BiSearchAlt />
-        </div>
       </div>
     </>
   );
