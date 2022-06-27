5545c933fca21a2751a36680ddd5417ad257a9ea
Natalia <nataliasierra0404@gmail.com>
2022-06-27 16:32:36 -0500

    Se añade Menú Hamburguesa

--- src/Components/Layout/NavBar/NavBar.jsx
+++ src/Components/Layout/NavBar/NavBar.jsx
@@ -15,6 +15,11 @@ import { UIModalNotification } from '../../UI/Notification-UI-Component/UIModalN
 import { HiOutlineLogout } from "react-icons/hi";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
 import { AiOutlineMenu } from "react-icons/ai";
+import { BsWindowSidebar } from 'react-icons/bs';
+import { TiMessages } from 'react-icons/ti';
+
+
+
 /* ---------------------- MODAL LOGIC ------------------------ */ 
 
 function rand() {
@@ -76,6 +81,13 @@ export const NavBar = () => {
     setOpen(false)
   }
 
+  const handleClickMenu = () =>{
+    console.log("Click")
+    const buttonTogle = document.querySelector(".navbar-toggler")
+    BsWindowSidebar.click(buttonTogle)
+    
+  }
+
   const body = (
     <div className='modalWindowFeatures'>
       <div style={modalStyle} className="cont-modal-notification">
@@ -91,6 +103,7 @@ export const NavBar = () => {
     <div className="primaryHeader">
       <div className="headerLogotype">
         <UILogos />
+        <h2 className="titleLogo">FULLMARKET</h2>
       </div>
       <div className="primaryBtnHeader">
         <div className="buttons-navbar">
@@ -116,60 +129,66 @@ export const NavBar = () => {
             </NavLink>
           )}
           {UserInSesion ? (
-          <>
-          <AiOutlineMenu className="btn btn-primary btn-primaryHeader" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" ></AiOutlineMenu>
-          <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">FullMarket</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-              {UserInSesion ? (
-              <NavLink to="/LoggedUser/MyProducts">
-                <UIButtons
-                  classButtons="btn-primaryHeader"
-                  nameButtons="Mis Productos"
-                ></UIButtons>
-              </NavLink>
-              ) : (
-                ''
-              )}
-          
-              {UserInSesion ? 
-              <NavLink to="/">
-                <HiHome className="home-icon" />
-              </NavLink>
-              : ''
-              }
+          <nav className="navbar navbar-expand-lg navbar-light cont-nav-desplegable">
+            <div className="container-fluid navDesplegable">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+              </button>
 
-              {UserInSesion && (
-                <NavLink to="/LoggedUser/AddProduct" className="nav-link">
-                  <MdOutlineAddShoppingCart className="btn-Add"/>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                {UserInSesion ? (
+                <NavLink to="/LoggedUser/MyProducts"
+                  onClick={handleClickMenu}>
+                  <UIButtons
+                    classButtons="btn-primaryHeader"
+                    nameButtons="Mis Productos"
+                  ></UIButtons>
                 </NavLink>
-              )}
-              {UserInSesion && (
-                <a>
-                  <AiFillBell className='notification-icon' onClick={handleModalOpen} />
-                </a>
-              )}
-              {UserInSesion && (
-                <a>
-                  <NavLink to="/LoggedUser/PrivateInformation/ChatHistory" className="nav-link">
-                    <AiOutlineSend className='notification-icon' />
+                ) : (
+                  ''
+                )}
+            
+                {UserInSesion ? 
+                <NavLink to="/"
+                  onClick={handleClickMenu}>
+                  <HiHome className="home-icon" />
+                </NavLink>
+                : ''
+                }
+
+                {UserInSesion && (
+                  <NavLink to="/LoggedUser/AddProduct" className="nav-link"
+                  onClick={handleClickMenu}>
+                    <MdOutlineAddShoppingCart className="btn-Add"/>
                   </NavLink>
-                </a>
-              )} 
-              {UserInSesion ? (
-                <a>
-                  <HiOutlineLogout  onClick={()=>Logout(navigate)} className="btn-Logout"  />
-                </a>
-                  // <p  onClick={()=>Logout(navigate)} className="btn-Logout">Cerrar Sesión</p>
-              ) : (
-                ''
-              )}
+                )}
+                {UserInSesion && (
+                  <a onClick={handleClickMenu}>
+                    <AiFillBell className='notification-icon' onClick={handleModalOpen} />
+                  </a>
+                )}
+                {UserInSesion && (
+                  <a>
+                    <NavLink to="/LoggedUser/PrivateInformation/ChatHistory" className="nav-link"
+                    onClick={handleClickMenu}>
+                      <TiMessages className='notification-icon' />
+                    </NavLink>
+                  </a>
+                )} 
+                {UserInSesion ? (
+                  <a>
+                    <HiOutlineLogout  onClick={()=>Logout(navigate)} className="btn-Logout"  />
+                  </a>
+                    // <p  onClick={()=>Logout(navigate)} className="btn-Logout">Cerrar Sesión</p>
+                ) : (
+                  ''
+                )}
+
+              </div>
             </div>
-          </div> 
-          </>
+
+          </nav>
           )
           :
           ""
