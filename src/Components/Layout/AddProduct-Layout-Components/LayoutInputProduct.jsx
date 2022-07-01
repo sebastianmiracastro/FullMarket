ed9ff608f1d936c8670464c93a3a8684d4730cc8
N_Sierra <nataliasierra0404@gmail.com>
2022-07-01 10:59:35 -0500

    Se organiza modal

--- src/Components/Layout/AddProduct-Layout-Components/LayoutInputProduct.jsx
+++ src/Components/Layout/AddProduct-Layout-Components/LayoutInputProduct.jsx
@@ -8,8 +8,8 @@ function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
